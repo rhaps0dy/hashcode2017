dbed2431fec6f5b806b2dd7b74b9f6f738a5ddf9
Adrià Garriga <adria.garriga@gmail.com>
2017-02-23 19:13:55 +0000

    Parece hecho

--- main.py
+++ main.py
@@ -1,31 +1,96 @@
 #!/usr/bin/env python3
 
 import sys
+import numpy as np
 
 def intify(line):
     l = line.strip().split()
     return list(int(i) for i in l)
 
+class Problem:
+    def __init__(self, videos, endpoints, requests, cache_capacity, n_cache_servers):
+        self.video_sizes = videos
+        self.endpoints = endpoints
+        self.requests = requests
+        self.cache_capacity = cache_capacity
+
+        self.caches = list({'videos': set(), 'capacity_left': 0, 'endpoints': []} for _ in range(n_cache_servers))
+        for e in self.endpoints:
+            for c_id, c_lat in e['caches'].items():
+                self.caches[c_id]['endpoints'].append({'endpoint': e, 'latency': c_lat})
+
+        self.requests_by_video = list([] for _ in range(len(self.videos)))
+        for r in self.requests:
+            self.requests_by_video[r['video_i']].append(r)
+
+        self.density_matrix = np.zeros((len(self.caches), len(self.video_sizes)))
+        self.requests_by_cache_video = {}
+        for vid, reqs in enumerate(self.requests_by_video):
+            for r in reqs:
+                for cache in self.endpoints[r['endpoint_i']]['caches']:
+                    t = (cache, vid)
+                    if t not in self.requests_by_cache_video:
+                        self.requests_by_cache_video[t] = []
+                    self.requests_by_cache_video[t].append(r)
+
+        self.solve_problem()
+
+    def solve_problem(self):
+        for cache in range(self.density_matrix[0]):
+            for video in range(self.density_matrix[1]):
+                self.density_matrix[cache,video] = self.video_density(cache, video)
+
+        col_left = np.ones(shape=[self.density_matrix.shape[1]], dtype=np.bool)
+        while np.any(col_left):
+            max_cache, max_video = np.unravel_index(np.argmax(self.density_matrix), self.density_matrix.shape)
+            self.caches[max_cache]['videos'].add(max_video)
+            self.density_matrix[max_cache, max_video] = 0
+            to_update, = np.where(self.density_matrix[max_cache,:] != 0.0)
+            for video in to_update:
+                self.density_matrix[max_cache, video] = self.video_density(max_cache, video)
+            if len(to_update) == 0:
+                col_left[video] = False
+
+    def video_density(self, cache, video):
+        video_size = self.video_sizes[video]
+        if video_size > self.cache_left_capacity[cache]:
+            return 0.0
+
+        sum_densities = 0
+        for r in self.requests_by_cache_video[cache, video]:
+            endp = self.endpoints[r['endpoint_i']]
+            min_latency = endp['latency']
+            for c_id, c_lat in endp['caches'].items():
+                if video in self.caches[c_id]['videos']:
+                    min_latency = min(min_latency, c_lat)
+            current_lat = endp['caches'][cache]
+            if current_lat >= min_latency:
+                continue
+            sum_densities += (min_latency - current_lat) * r['n']
+
+        return sum_densities / video_size
+
+
 def main():
     with open(sys.argv[1], 'r') as f:
         ls = f.readlines()
-    _, n_ends, _, _, cache_capacity = intify(ls[0])
+    n_videos, n_ends, n_requests, n_cache_servers, cache_capacity = intify(ls[0])
     videos = intify(ls[1])
     ls = ls[2:]
     endpoints = []
     for e in range(n_ends):
         latency, n_caches = intify(ls[0])
-        d = {'latency': latency, 'caches': []}
+        d = {'latency': latency, 'caches': {}}
         for c in range(n_caches):
-            d['caches'].append(tuple(intify(ls[1+c])))
+            c_id, c_lat = intify(ls[1+c])
+            d['caches'][c_id] = c_lat
         endpoints.append(d)
         ls = ls[1+n_caches:]
     requests = []
     for l in ls:
         video, endpoint, n = intify(l)
-        requests.append({'video': video, 'endpoint': endpoint, 'n': n})
-    del ls
-    print(videos, endpoints, requests, cache_capacity)
+        requests.append({'video_i': video, 'endpoint_i': endpoint, 'n': n})
+    p = Problem(videos, endpoints, requests, cache_capacity, n_cache_servers)
 
 if __name__ == '__main__':
     main()
